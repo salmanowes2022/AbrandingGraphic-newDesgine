210d8b642069122db8b15ef3c5ff6ba3bf36d90a
salman.awasise@students.alquds.edu <salman.awasise@students.alquds.edu>
2025-07-16 18:38:52 +0300

    new changes

--- src/components/CustomersSection.jsx
+++ src/components/CustomersSection.jsx
@@ -1,24 +1,31 @@
 import './CustomersSection.css';
-import translations from '../translations';
 
-const CustomersSection = ({ language }) => (
-  <section className='customers-section-container'>
-    <div className='customers-section-headline'>
-      <h2 className='customers-title'>{translations[language].customersTitle}</h2>
+const customers = [
+  ['PANGO', 'TRULLION', 'ELAL', 'PENLINK', 'RISE'],
+  ['ISRAEL HAYOM', 'BITUACH YASHIR', 'PRIMIS'],
+  ['BANK HAPOALIM', 'YELLOW', 'BANK JERUSALEM'],
+  ['BERESHIT HOTEL', 'HASBARA']
+];
+
+const CustomersSection = () => (
+
+<section className='customers-section-container'>
+  <div className='customers-section-headline'>
+    <h2 className='customers-title'>Costumers</h2>
+  </div>
+  <section className="customers-section">
+    <div className="customers-list">
+      {customers.map((group, i) => (
+        <div key={i} className="customers-row">
+          {group.map((name, idx) => (
+            <span key={name} className="customers-name">
+              {name}{idx !== group.length - 1 ? ',' : ''}
+            </span>
+          ))}
+        </div>
+      ))}
     </div>
-    <section className="customers-section">
-      <div className="customers-list">
-        {translations[language].customers.map((group, i) => (
-          <div key={i} className="customers-row">
-            {group.map((name, idx) => (
-              <span key={name} className="customers-name">
-                {name}{idx !== group.length - 1 ? ',' : ''}
-              </span>
-            ))}
-          </div>
-        ))}
-      </div>
-    </section>
+  </section>
   </section>
 );
 
